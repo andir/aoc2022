0abeb0b7c2320b1d5073fbc8599284dfb0d2d9e4
Andreas Rammhold <andreas@rammhold.de>
2022-12-02 11:14:32 +0100

    day02 part2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,11 +1,12 @@
 use smallvec::SmallVec;
 
 fn main() {
-    let input  = include_str!("input.txt");
+    let input = include_str!("input.txt");
     println!("part1: {}", part1(input));
+    println!("part1: {}", part2(input));
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Outcome {
     Lost,
     Draw,
@@ -47,12 +48,35 @@ impl Choice {
             _ => Outcome::Lost,
         }
     }
+
+    fn pick_for_outcome(&self, outcome: &Outcome) -> Choice {
+        match (self, outcome) {
+            (x, Outcome::Draw) => *x,
+            (Choice::Paper, Outcome::Win) => Choice::Scissors,
+            (Choice::Rock, Outcome::Win) => Choice::Paper,
+            (Choice::Scissors, Outcome::Win) => Choice::Rock,
+            (Choice::Paper, Outcome::Lost) => Choice::Rock,
+            (Choice::Rock, Outcome::Lost) => Choice::Scissors,
+            (Choice::Scissors, Outcome::Lost) => Choice::Paper,
+        }
+    }
 }
 
 fn run_round(own: Choice, other: Choice) -> u64 {
     let outcome = own.play(&other);
 
     outcome.score() + own.shape_score()
+}
+
+impl From<&str> for Outcome {
+    fn from(other: &str) -> Outcome {
+        match other {
+            "X" => Outcome::Lost,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
+            x => panic!("Unknown input: {}", x),
+        }
+    }
 }
 
 impl From<&str> for Choice {
@@ -74,11 +98,33 @@ fn decode_row(input: &str) -> (Choice, Choice) {
     (data[0], data[1])
 }
 
+fn decode_part2_row(input: &str) -> (Choice, Outcome) {
+    let (a, b) = input.split_at(input.find(' ').expect("All input lines have a space"));
+    let choice = Choice::from(a.trim());
+    let outcome = Outcome::from(b.trim());
+    (choice, outcome)
+}
+
 fn part1(input: &str) -> u64 {
     input
         .lines()
         .map(decode_row)
         .map(|(other, own)| run_round(own, other))
+        .sum()
+}
+
+fn run_part2_round(opponent_choice: Choice, outcome: Outcome) -> u64 {
+    let choice = opponent_choice.pick_for_outcome(&outcome);
+    assert_eq!(choice.play(&opponent_choice), outcome);
+
+    outcome.score() + choice.shape_score()
+}
+
+fn part2(input: &str) -> u64 {
+    input
+        .lines()
+        .map(decode_part2_row)
+        .map(|(other, outcome)| run_part2_round(other, outcome))
         .sum()
 }
 
@@ -90,5 +136,6 @@ mod tests {
 B X
 C Z"#;
         assert_eq!(super::part1(input), 15);
+	assert_eq!(super::part2(input), 12);
     }
 }
